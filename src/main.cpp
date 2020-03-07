f4b550dc8eeabed03d910f3ea342a7f9b1456a9a
Paul Smith <code@uvwxy.de>
2020-03-07 20:01:47 +0100

    wifi mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,103 +1,105 @@
-#include <ESP8266HTTPClient.h>
-#include <ESP8266WiFi.h>
+#include <Arduino.h>
+#include <RtcDS1307.h>
+#include <SPI.h>
 #include <U8g2lib.h>
 #include <Wire.h>
-#include "Arduino.h"
 #include "config.h"
+#include "mini-wifi.h"
 
-U8G2_SSD1306_128X32_UNIVISION_F_SW_I2C u8g2(U8G2_R0, D5, D4, D2);
-unsigned long timestamp_start = 0;
-unsigned long timestamp_diff = 0;
-int secs = 0;
-int mins = 0;
-int hours = 0;
-boolean start = true;
+#define countof(a) (sizeof(a) / sizeof(a[0]))
 
-const int button = D8;  // the number of the pushbutton pin
+RtcDS1307<TwoWire> Rtc(Wire);
+U8G2_PCD8544_84X48_F_4W_HW_SPI u8g2(U8G2_R2, /* cs=D8*/ 15, /* dc=D4*/ 2, /* reset=D3*/ 0);  // Nokia 5110 Display
 
-// variables will change:
-int buttonState = 0;  // variable for reading the pushbutton status
+MiniWifi wifi("client", WIFI_SSID, WIFI_PASS);
 
-void waitForWifi() {
-  WiFi.hostname(HOSTNAME);
-  WiFi.mode(WIFI_STA);
-  do {
-    WiFi.begin(WIFI_SSID, WIFI_PASS);
-    delay(4000);
-  } while (WiFi.status() != WL_CONNECTED);
-}
-
-void postData(String filename, String payload, boolean append, boolean tsprefix) {
-  HTTPClient http;
-  http.begin(String("http://") + MINI_IOT_SERVER         //
-             + "/files/" + HOSTNAME                      //
-             + "/" + filename                            //
-             + "?append=" + (append ? "true" : "false")  //
-             + "&tsprefix=" + (tsprefix ? "true" : "false"));
-  http.addHeader("Content-Type", "text/plain");
-  http.POST(payload);
-  // http.writeToStream(&Serial);
-  http.end();
-}
-
-String twoDigits(int x) { return (x < 10 ? String("0") : String("")) + String(x); }
-
-const String format(int hours, int mins, int secs) {
-  return twoDigits(hours) + String(":") + twoDigits(mins) + String(":") + twoDigits(secs);
-}
-
-void setup() {
-  Serial.begin(115200);
-
-  pinMode(button, INPUT);
-
+void setup(void) {
+  Serial.begin(9600);
+  wifi.setDebugStream(&Serial);
   u8g2.begin();
-  u8g2.setContrast(0);
+  Wire.begin(D2, D1);
 
-  u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_profont12_tf);
-  u8g2.drawStr(1, 10, "Connecting...");
-  u8g2.sendBuffer();
+  if (Rtc.LastError() != 0) {
+    Serial.print("RTC communications error = ");
+    Serial.println(Rtc.LastError());
+  }
 
-  waitForWifi();
+  // Rtc.SetDateTime(RtcDateTime(__DATE__, __TIME__));
+
+  if (!Rtc.GetIsRunning()) {
+    Rtc.SetIsRunning(true);
+  }
+}
+void printDayOfWeek(const RtcDateTime& dt, U8G2& u8g2) {
+  u8g2.setFont(u8g2_font_profont10_tf);
+  switch (dt.DayOfWeek()) {
+    case 1:
+      u8g2.drawStr(4, 44, "Monday");
+      break;
+    case 2:
+      u8g2.drawStr(4, 44, "Tuesday");
+      break;
+    case 3:
+      u8g2.drawStr(4, 44, "Wednesday");
+      break;
+    case 4:
+      u8g2.drawStr(4, 44, "Thursday");
+      break;
+    case 5:
+      u8g2.drawStr(4, 44, "Friday");
+      break;
+    case 6:
+      u8g2.drawStr(4, 44, "Saturday");
+      break;
+    default:
+      u8g2.drawStr(4, 44, "Sunday");
+  }
 }
 
-void loop() {
-  buttonState = digitalRead(button);
+void printDate(const RtcDateTime& dt, U8G2& u8g2) {
+  char datestring[11];
+  snprintf_P(datestring, countof(datestring), PSTR("%04u/%02u/%02u"), dt.Year(), dt.Month(), dt.Day());
+  u8g2.setFont(u8g2_font_profont12_tf);
+  u8g2.drawStr(12, 11, datestring);
+}
 
-  if (WiFi.status() != WL_CONNECTED) {
-      // reconnect if connection lost
-      waitForWifi();
-  }
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    if (start) {
-      timestamp_start = millis();
-      postData("phone.csv", "0", true, true);
-      postData("phone.csv", "1", true, true);
-      start = false;
-    }
-
-    timestamp_diff = millis() - timestamp_start;
-    secs = (int)(timestamp_diff / 1000) % 60;
-    mins = (int)((timestamp_diff / (1000 * 60)) % 60);
-    hours = (int)((timestamp_diff / (1000 * 60 * 60)) % 24);
-    Serial.println(format(hours, mins, secs));
+void printTime(const RtcDateTime& dt, U8G2& u8g2) {
+  char datestring[6];
+  if (dt.Second() % 2 == 0) {
+    snprintf_P(datestring, countof(datestring), PSTR("%02u:%02u"), dt.Hour(), dt.Minute());
   } else {
-    if (!start) {
-      postData("phone.csv", "1", true, true);
-      postData("phone.csv", "0", true, true);
-    }
-    start = true;
-    Serial.println("Low");
+    snprintf_P(datestring, countof(datestring), PSTR("%02u %02u"), dt.Hour(), dt.Minute());
   }
+  u8g2.setFont(u8g2_font_profont29_tf);
+  u8g2.drawStr(3, 34, datestring);
+}
+
+void printSignalStrength(uint8_t quality, U8G2& u8g2) {
+  u8g2.setFont(u8g2_font_profont10_tf);
+  u8g2.drawStr(60, 44, String(quality).c_str());
+}
+
+void drawSignal(U8G2& u8g2, uint8_t x, uint8_t y, uint8_t strength) {
+  for (uint8_t i = 0; i < strength; i++) {
+    u8g2.drawCircle(x, y, i * 3, U8G2_DRAW_UPPER_LEFT);
+  }
+}
+
+void loop(void) {
+  RtcDateTime time = Rtc.GetDateTime();
 
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_profont12_tf);
-  u8g2.drawStr(1, 10, "Smartphone Timer v1.0");
-  u8g2.setFont(u8g2_font_profont22_tf);
-  u8g2.drawStr(16, 27, (format(hours, mins, secs)).c_str());
+
+  printDayOfWeek(time, u8g2);
+  printDate(time, u8g2);
+  printTime(time, u8g2);
+  if (wifi.isConnected()) {
+    drawSignal(u8g2, 81, 45, wifi.getSignalQuality() / 20);
+  }
+  u8g2.drawRFrame(0, 0, 84, 48, 3);
+
   u8g2.sendBuffer();
-  delay(1000);
+
+  wifi.checkWifi();
+  delay(1000 / 2);
 }
